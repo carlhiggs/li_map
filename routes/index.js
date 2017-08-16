311fe8a8fe9b3bc77abbddcfb7f3767abf80da69
Carl Higgs <carl.higgs@rmit.edu.au>
2017-08-16 14:06:12 +1000

    substantial update including authorisation password requirement, raw scores with interval, and heterogeneity estimate

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express'); // require Express
+var basicAuth = require('basic-auth');
 var router = express.Router(); // setup usage of the Express router engine
 // var template = require('jade').compileFile(__dirname + '/../views/index.jade')
 
@@ -7,13 +8,7 @@ var router = express.Router(); // setup usage of the Express router engine
 var pg = require("pg"); // require Postgres module
 
 // Setup connection
-// var username = "spatial@ligres" // sandbox username
-// var password = "sp$nUggy" // read only privileges on our table
-// var host = "ligres.postgres.database.azure.com:5432"
-// var database = "ligres" // database name
-// var ssl = "
-// var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
-// var conString = spatial@ligres:sp$nUggy@ligres.postgres.database.azure.com:5432/ligres?ssl=true
+
 var client = new pg.Client({
   user: "spatial@ligres",
   password: "sp$nUggy",
@@ -23,40 +18,40 @@ var client = new pg.Client({
   ssl: true
 });
 
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === 'hlc' && user.pass === 'p7lo!') {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
 
 // Set up your database query to display GeoJSON
-var li_query = "SELECT * FROM li_map_json_hard";
+// var li_query = "SELECT * FROM li_map_json_hard";
+var li_query = "SELECT * FROM li_map_json_h_mini";
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
   res.render('index', { title: 'Pilot Liveability Index' });
-  // try {
-    // var html = template({ title: 'Pilot Liveability Index' })
-    // res.render(html)
-  // } catch (e) {
-    // next(e)
-  // }
 });
 
 module.exports = router;
 
-// /* GET Postgres JSON data */
-// router.get('/data', function (req, res) {
-    // // var client = new pg.Client(conString);
-    // client.connect();
-    // var query = client.query(li_query);
-    // query.on("row", function (row, result) {
-        // result.addRow(row);
-    // });
-    // query.on("end", function (result) {
-        // res.send(result.rows[0].row_to_json);
-        // res.end();
-    // });
-// });
 
 /* GET the map page */
-router.get('/map', function(req, res) {
+router.get('/map', auth, function(req, res) {
     // var client = new pg.Client(conString);
     client.connect();
     var query = client.query(li_query);
